973e5202ab144b1c5dc2fc985ca903d7434dacec
DtThanhTuyen-mu <trieubao91848@gmail.com>
2023-06-06 09:51:25 +0700

    Add icon one times

--- restaurant/src/components/NavbarStaff.js
+++ restaurant/src/components/NavbarStaff.js
@@ -1,9 +1,11 @@
-import { AppBar, Tab, Tabs, Toolbar, Typography } from "@mui/material";
+import { AppBar, Tab, Tabs, Toolbar } from "@mui/material";
 import { styled } from "@mui/system";
 import React, { useState } from "react";
 import MenuIcon from "@mui/icons-material/Menu";
 import { Link, Outlet } from "react-router-dom";
-import LayoutOutline from "../pages/outline/LayoutOutline";
+import NoteAltIcon from "@mui/icons-material/NoteAlt";
+import { faMap, faReceipt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
@@ -14,7 +16,7 @@ const StyledToolbar = styled(Toolbar)({
 const NavbarStaff = () => {
   const [value, setValue] = useState(0);
   const handleTabs = (e, value) => {
-    // console.log(value);
+    console.log(value);
     setValue(value);
   };
   return (
@@ -22,31 +24,43 @@ const NavbarStaff = () => {
       <AppBar position="static">
         <StyledToolbar>
           <MenuIcon />
-          {/* <Typography variant="h6">Restaurant TTT</Typography> */}
-
           <Tabs
             indicatorColor="secondary"
             textColor="inherit"
-            // sx={{backgroundColor:'red'}}
             value={value}
             onChange={handleTabs}
+            aria-label="nav tabs example"
           >
+
             <Link to={"/staff/orders"}>
-              <Tab label="Đặt món"></Tab>
+              <Tab
+                icon={<NoteAltIcon />}
+                iconPosition="start"
+                label="Đặt món"
+              ></Tab>
             </Link>
-            <Link to={"outline"}>
-              <Tab label="Sơ đồ"> </Tab>
+
+            <Link to={"/staff/outline"}>
+              <Tab
+                icon={<FontAwesomeIcon icon={faMap} />}
+                iconPosition="start"
+                label="Sơ đồ"
+              ></Tab>
             </Link>
-            <Tab label="Trả món"> </Tab>
-            <Tab label="Phiếu tạm tính"> </Tab>
+
+            {/* <Tab label="Trả món"> </Tab> */}
+
+            <Tab
+              icon={<FontAwesomeIcon icon={faReceipt} />}
+              iconPosition="start"
+              label="Phiếu tạm tính"
+            ></Tab>
           </Tabs>
         </StyledToolbar>
       </AppBar>
       <Outlet />
-   
-      {/* <TabPanel value={value} index={0}>
-        
-      </TabPanel>
+
+      {/* <TabPanel value={value} index={0}></TabPanel>
       <TabPanel value={value} index={1}>
         <LayoutOutline />
       </TabPanel>
@@ -56,6 +70,7 @@ const NavbarStaff = () => {
       <TabPanel value={value} index={3}>
         Item 4 Detail
       </TabPanel> */}
+      
     </div>
   );
 };
