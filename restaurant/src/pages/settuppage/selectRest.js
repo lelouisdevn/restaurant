39af6d6ee4d699df80a83eeb9a3b008894a0d324
lelouisdevn <atlanteansvietnam@outlook.com>
2023-06-13 21:01:41 +0700

    Add 'loading' icon after selecting a restaurant, any of Thu Vo's logic operations are not affected.

--- restaurant/src/pages/settuppage/selectRest.js
+++ restaurant/src/pages/settuppage/selectRest.js
@@ -2,6 +2,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
 import React, { useState, useEffect } from "react";
+import Loading from "../products/Loading";
+import '../products/this.css';
 import axios from "axios";
 const SelectRest = () => {
     const navigator = useNavigate();
@@ -25,24 +27,30 @@ const SelectRest = () => {
 
     const getRest = async (id) =>{
         await axios
-        .get(`http://localhost:4000/api/getallrestfromone/id=${id}`)
+            .get(`http://localhost:4000/api/getallrestfromone/id=${id}`)
         .then((res) =>{
-            const temp = res?.data.rest;
-            setRest(temp);
-            //console.log(temp);
-        })
+                const temp = res?.data.rest;
+                setRest(temp);
+                //console.log(temp);
+            })
         .catch((error) =>{
             console.log("Error: ",error);
-        }) 
-        .finally(() => {
-            setisLoading(false);
-        }); 
-        
+            })
+            .finally(() => {
+                setisLoading(false);
+            });
+
     }
-    const handleRestClick = async(id) =>{
-        localStorage.setItem("RestaurantID",id);
-        navigate("/manage/home");
-        
+    const [isSelected, setSelected] = useState(false);
+    const handleRestClick = async (id) => {
+        localStorage.setItem("RestaurantID", id);
+
+        //Wait 3s before going to 'home';
+        setSelected(!isSelected);
+        setTimeout(() => {
+            navigate("/manage/home");
+        }, 3000);
+
     }
 
 
@@ -53,20 +61,24 @@ const SelectRest = () => {
     */
     return (
         <>
-            <div>
-                Vui lòng chọn 1 nhà hàng để bắt đầu!
-            </div>
-            <div className='main-content'>
-                {
+            {isSelected ? <Loading message={"Đang tải xuống dữ liệu..."} /> :
+                <>
+                    <div>
+                        Vui lòng chọn 1 nhà hàng để bắt đầu!
+                    </div>
+                    <div className='main-content'>
+                        {
                     isLoading? null : rest.map((row)=>(
                         <div><button onClick={(e)=>handleRestClick(row.info._id)} type="button">{row.info.rest_name}</button></div>
-                    ))
-                }
-            </div>
-            <div className='footer' onClick={createRest}>
-                <FontAwesomeIcon icon={faGear} />
-                <> Thêm nhà hàng khác</>
-            </div>
+                            ))
+                        }
+                    </div>
+                    <div className='footer' onClick={createRest}>
+                        <FontAwesomeIcon icon={faGear} />
+                        <> Thêm nhà hàng khác</>
+                    </div>
+                </>
+            }
         </>
     )
 }
