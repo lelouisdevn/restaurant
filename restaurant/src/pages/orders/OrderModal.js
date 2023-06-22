2a09ba33eb655c969505134d62312bcf3c6f1ee2
lelouisdevn <atlanteansvietnam@outlook.com>
2023-06-22 16:58:32 +0700

    Update order QR code

--- restaurant/src/pages/orders/OrderModal.js
+++ restaurant/src/pages/orders/OrderModal.js
@@ -30,15 +30,15 @@ const OrderModal = (props) => {
         "content": "ttoan",
         "third": "6304c7c3"
     };
-    var qrcode =  bankAccount.first + bankAccount.n_char + bankAccount.money 
-                + bankAccount.second + bankAccount.content + bankAccount.third;
+    var qrcode = bankAccount.first + bankAccount.n_char + bankAccount.money
+        + bankAccount.second + bankAccount.content + bankAccount.third;
     var qrcode = qrcode.toString();
     console.log(qrcode);
-    
-     const json = localStorage.getItem("infoRestaurant");
-     const valuejson = JSON.parse(json);
+
+    const json = localStorage.getItem("infoRestaurant");
+    const valuejson = JSON.parse(json);
     console.log(valuejson);
-     const [infoStaff, setInfoStaff] = useState(valuejson);
+    const [infoStaff, setInfoStaff] = useState(valuejson);
     // Set criteria to 1 and set display status of modal to false;
     const handlePay = () => {
         props.changeCriteria();
@@ -59,14 +59,14 @@ const OrderModal = (props) => {
     const getDetails = async () => {
         const url = `${HOST}/order/${orderId}/details`;
         const res = await axios.get(url)
-                // .then((res) => {
-                //     console.log(res?.data.details);
-                //     setOrderDetails(res?.data.details);
-                // })
+        // .then((res) => {
+        //     console.log(res?.data.details);
+        //     setOrderDetails(res?.data.details);
+        // })
         var data = [];
         if (res.status === 200) {
-          data = res.data.details;
-          console.log(data);
+            data = res.data.details;
+            console.log(data);
         }
         if (data.length != 0) {
             data = data.filter((item) => item.status != "xoa");
@@ -152,7 +152,7 @@ const OrderModal = (props) => {
                                     </td>
                                 </tr>
                                 <tr><td>Mã hóa đơn:</td><td>{order._id}</td></tr>
-                                <tr><td>Đặt lúc:</td><td>{new Date(order.order_at).toLocaleString("vi-VN", {hour12: false})}</td></tr>
+                                <tr><td>Đặt lúc:</td><td>{new Date(order.order_at).toLocaleString("vi-VN", { hour12: false })}</td></tr>
                                 <tr><td>Thanh toán lúc:</td><td>{new Date().toLocaleString("vi-VN", { hour12: false })}</td></tr>
                                 <tr>
                                     <td>Bàn:</td>
@@ -164,8 +164,8 @@ const OrderModal = (props) => {
                                 <tr><td>Nhân viên:</td><td>{user.staff_name}</td></tr>
                             </table>
                             <table style={{ fontSize: "16px" }}>
-                                <tr>
-                                    <td style={{ borderRadius: "10px 0 0 0!important" }}>STT</td>
+                                <tr style={{fontWeight: "bold"}}>
+                                    <td>STT</td>
                                     <td>Tên sản phẩm</td>
                                     <td>Số lượng</td>
                                     <td>Đơn giá</td>
@@ -183,19 +183,22 @@ const OrderModal = (props) => {
                                 <tr>
                                     <td colSpan={4}>Tổng:</td>
                                     <td
-                                        style={{fontWeight: "bold"}}
+                                        style={{ fontWeight: "bold" }}
                                     >{VND.format(order.total)}</td>
                                 </tr>
                             </table>
-                            {/* <center> */}
-                            <div>
-                                <QRCode style={{width: "150px", margin: "0 auto"}} value={qrcode} />
-                            </div>
-                            {/* </center> */}
                         </div>
-
+                        <div style={{fontSize: "18px"}}>
+                            
+                            <QRCode
+                                style={{ width: "160px", height: "160px", margin: "0 auto", padding: "10px", border: "solid 1px grey", borderRadius: "10px" }} 
+                                value={qrcode} />
+                            <div>Mã QR thanh toán cho đơn hàng</div>
+                            <div style={{color: "crimson"}}>{order._id}</div>
+                            <div>{new Date().toLocaleString("vi-VN")}</div>
+                        </div>
                         <div style={{ fontSize: "16px" }}>
-                            <div>
+                            {/* <div>
                                 Tiền khách đưa:
                                 <input
                                     style={{ border: "solid 1px lightgrey", borderRadius: "10px", padding: "5px 7px", margin: "0 7px" }}
@@ -203,7 +206,7 @@ const OrderModal = (props) => {
                             </div>
                             <div>
                                 Tiền thối: {VND.format(money - order.total)}
-                            </div>
+                            </div> */}
                         </div>
                     </div>
                     <div className="order-modal-footer">
