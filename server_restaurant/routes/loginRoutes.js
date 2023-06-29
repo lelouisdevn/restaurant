eb4b20ffab4ad83737b52a2343637983a21f7a87
AnnaSarah <loancuckeoroi9899@gmail.com>
2023-06-29 15:29:27 +0700

    Handling error of wrong login incredentials

--- server_restaurant/routes/loginRoutes.js
+++ server_restaurant/routes/loginRoutes.js
@@ -6,7 +6,7 @@ const UserRestDetail = require("../models/UserRestDetail");
 router.post("/login", async (req, res) => {
     
     try {
-        let login = await User.find({username: req.body.username,password: req.body.password});
+        let login = await User.find({username: req.body.username,password: req.body.password, staff_status: "1"});
             res.send({ login });
             //console.log(login);
     } catch (error) {
