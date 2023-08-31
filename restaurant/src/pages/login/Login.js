bdaac0cf57d876629a0ce9ed953c2bacc072f9f5
lelouisdevn <atlanteansvietnam@outlook.com>
2023-08-31 23:19:49 +0700

    Create new login form

--- restaurant/src/pages/login/Login.js
+++ restaurant/src/pages/login/Login.js
@@ -9,7 +9,7 @@ import '../settuppage/setup.css';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBuilding, faClose, faComputer, faEarth, faEarthAsia, faSolarPanel, faSun, faUserAstronaut, faUserFriends } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faBuilding, faClose, faComputer, faEarth, faEarthAsia, faPaperPlane, faPen, faPlusCircle, faShare, faSolarPanel, faSun, faUserAstronaut, faUserFriends } from "@fortawesome/free-solid-svg-icons";
 // import { useState } from "react";
 const Login = (props) => {
   const navigate = useNavigate();
@@ -28,6 +28,11 @@ const Login = (props) => {
   // },[]);
   console.log("link ne: ", localStorage.getItem('currentUrl'));
   const [float, setFloat] = useState("");
+  const signUp = () => {
+    const signUpURL = "/business/sign-up";
+    navigate(signUpURL);
+    // window.location.href = signUpURL;
+  }
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -136,133 +141,167 @@ const Login = (props) => {
         </div>
       </div>
       <section className="h-screen">
-        <div className="h-full" style={{height: "calc(100vh - 150px)"}}>
+        <div className="h-full" style={{ height: "calc(100vh - 150px)" }}>
           <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
             <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
-              <div>
+              <div style={{margin: "7px 15px"}}>
                 <div style={{ fontSize: "30px" }}>Giải pháp cung cấp phần mềm quản lý nhà hàng</div>
                 <div style={{ fontSize: "20px", margin: "10px" }}>Trải nghiệm ngay hôm nay!</div>
-                <div>
+                {/* <div>
                   <button className='updateButton' onClick={() => {
                     // navigate("/login")
                     // window.location.href = '/login';
                     setLoginStatus(true);
                   }}>Đăng nhập</button>
-                  <button className='normal'>Đăng ký</button>
+                  <button className='normal' onClick={() => signUp()}>Đăng ký</button>
+                </div> */}
+                <div className="footer" style={{ textAlign: "center", margin: "0 auto", width: "80%" }}>
+                  <div onClick={() => {
+                    setLoginStatus(true);
+                  }}>
+                    <Link>
+                      <FontAwesomeIcon icon={faPlusCircle} />
+                      <> Đăng nhập</>
+                    </Link>
+                  </div>
+                  <div>
+                    <Link>
+                      <FontAwesomeIcon icon={faPen} />
+                      <> Đăng ký</>
+                    </Link>
+                  </div>
                 </div>
               </div>
             </div>
             {isLoginButtonClicked ?
-            <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
-              <form onSubmit={handleLogin}>
-                <div className="w-full sm:w-2/3 lg:2/3 px-6 bg-blue-600 bg-clip-padding backdrop-filter backdrop-blur-sm text-white z-50 py-4  rounded-lg" style={{ background: "whitesmoke", border: "solid 1px lightgrey", filter: "drop-shadow(1px 1px 6px lightgrey)" }}>
-                  <FontAwesomeIcon icon={faClose} style={{position: "absolute", top: "10px", right: "10px", fontSize: "15px", color: "black"}} 
-                    onClick={() => {
-                      setLoginStatus(false);
-                    }}
-                  />
-                  <div className="w-full flex justify-center text-[#fff] text-3xl font-bold mb:2 md:mb-5" style={{ color: "black" }}>
-                    Đăng nhập
-                  </div>
-
-                  {/* {msgerr ? (
-                  <div className="mb-6 bg-green-200 flex justify-center items-center">
-                    <span className="py-1 text-black">{msgerr}</span>
-                  </div>
-                              ) : null} */}
-
-                  {/* <div className="mb-6">
-                  <label
-                    htmlFor="restaurant"
-                    className="block mb-2 text-sm font-medium text-white"
-                  >
-                    Tên nhà hàng
-                  </label>
-                  <input
-                    type="restaurant"
-                    id="restaurant"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 md:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    value={restaurant}
-                    onChange={(e) => {
-                      setRestaurant(e.target.value);
-                    }}
-                    required
-                  />
-                </div> */}
-                  <div className="mb-6">
-                    <label
-                      htmlFor="username"
-                      className="block mb-2 text-sm font-medium text-black text-left"
-                    >
-                      Tên đăng nhập
-                    </label>
-                    <input
-                      type="username"
-                      id="username"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 md:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="username"
-                      value={username}
-                      onChange={(e) => {
-                        setUsername(e.target.value);
+              <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12">
+                <form onSubmit={handleLogin} className="login-form-container">
+                <FontAwesomeIcon icon={faClose} style={{ position: "absolute", top: "10px", right: "10px", fontSize: "15px", color: "black" }}
+                      onClick={() => {
+                        setLoginStatus(false);
                       }}
-                      required
                     />
-                  </div>
-                  <div className="mb-6">
-                    <label
-                      htmlFor="password"
-                      className="block mb-2 text-sm font-medium text-black text-left"
-                    >
-                      Mật khẩu
-                    </label>
-                    <div className="flex">
-                      <input
-                        type={PorT ? "password" : "text"}
-                        id="password"
-                        className="relative bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 md:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        placeholder="*******"
-                        value={password}
+                  <div>
+                    <div className="login-form-title">
+                      <span>Đăng nhập</span>
+                      <br/><span style={{fontSize: "25px"}}>với tên đăng nhập và mật khẩu</span>
+                    </div>
+                    <div className="footer login-form">
+                      <div>
+                        <input placeholder="Tên đăng nhập:..."
+                        id="username" 
+                        type="username"
+                        value={username}
                         onChange={(e) => {
-                          setPassword(e.target.value);
+                          setUsername(e.target.value);
+                        }}
+                        required
+                        />
+                      </div>
+                      <div>
+                        <input placeholder="Mật khẩu:..." 
+                          id="password"
+                          type="password"
+                          value={password}
+                          onChange={(e) => {
+                            setPassword(e.target.value);
+                          }}
+                          required
+                        />
+                      </div>
+                      <div>
+                        <button style={{marginLeft: "0", padding: "0", width: "100%", height: "100%"}}>
+                        <FontAwesomeIcon icon={faPaperPlane} style={{color: "blue", fontSize: "20px"}} />
+                        {/* dn */}
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+                {/* <form onSubmit={handleLogin}>
+                  <div className="w-full sm:w-2/3 lg:2/3 px-6 bg-blue-600 bg-clip-padding backdrop-filter backdrop-blur-sm text-white z-50 py-4  rounded-lg" style={{ background: "whitesmoke", border: "solid 1px lightgrey", filter: "drop-shadow(1px 1px 6px lightgrey)" }}>
+                    <FontAwesomeIcon icon={faClose} style={{ position: "absolute", top: "10px", right: "10px", fontSize: "15px", color: "black" }}
+                      onClick={() => {
+                        setLoginStatus(false);
+                      }}
+                    />
+                    <div className="w-full flex justify-center text-[#fff] text-3xl font-bold mb:2 md:mb-5" style={{ color: "black" }}>
+                      Đăng nhập
+                    </div>
+                    <div className="mb-6">
+                      <label
+                        htmlFor="username"
+                        className="block mb-2 text-sm font-medium text-black text-left"
+                      >
+                        Tên đăng nhập
+                      </label>
+                      <input
+                        type="username"
+                        id="username"
+                        className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 md:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        placeholder="username"
+                        value={username}
+                        onChange={(e) => {
+                          setUsername(e.target.value);
                         }}
                         required
                       />
-                      {PorT ? (
-                        <VisibilityOffIcon
-                          className="icon relative text-black mt-2 ml-[-30px]"
-                          onClick={() => setPorT(false)}
-                        />
-                      ) : (
-                        <VisibilityIcon
-                          className="icon relative text-black mt-2 ml-[-30px]"
-                          onClick={() => setPorT(true)}
-                        />
-                      )}
                     </div>
-                  </div>
-                  <div className="flex flex-row justify-between">
-                    <div className="text-[#00FF00] text-sm md:text-md"></div>
-                  </div>
-                  <button
-                    className="mt-4  w-full flex justify-center text-sm md:text-xl
+                    <div className="mb-6">
+                      <label
+                        htmlFor="password"
+                        className="block mb-2 text-sm font-medium text-black text-left"
+                      >
+                        Mật khẩu
+                      </label>
+                      <div className="flex">
+                        <input
+                          type={PorT ? "password" : "text"}
+                          id="password"
+                          className="relative bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 md:p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                          placeholder="*******"
+                          value={password}
+                          onChange={(e) => {
+                            setPassword(e.target.value);
+                          }}
+                          required
+                        />
+                        {PorT ? (
+                          <VisibilityOffIcon
+                            className="icon relative text-black mt-2 ml-[-30px]"
+                            onClick={() => setPorT(false)}
+                          />
+                        ) : (
+                          <VisibilityIcon
+                            className="icon relative text-black mt-2 ml-[-30px]"
+                            onClick={() => setPorT(true)}
+                          />
+                        )}
+                      </div>
+                    </div>
+                    <div className="flex flex-row justify-between">
+                      <div className="text-[#00FF00] text-sm md:text-md"></div>
+                    </div>
+                    <button
+                      className="mt-4  w-full flex justify-center text-sm md:text-xl
                    bg-orange-600
                  py-2 rounded-md text-white"
-                    style={{ background: "#ff315c", margin: "7px 0" }}
-                  >
-                    Đăng nhập
-                  </button>
+                      style={{ background: "#ff315c", margin: "7px 0" }}
+                    >
+                      Đăng nhập
+                    </button>
 
 
-                </div>
-              </form>
-              <ToastContainer />
-            </div>
-            : 
-            <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12" style={{position: "relative"}}>
-              <div style={{fontSize: "20px", border:"solid 1px black",borderRadius: "10px", padding: "5px 20px", position: "absolute", left: "30%"}}>
-                Hi!</div>
-              <FontAwesomeIcon icon={faUserAstronaut} style={{fontSize: "200px", color: "#1976d2"}} />
-            </div>
+                  </div>
+                </form> */}
+                <ToastContainer />
+              </div>
+              :
+              <div className="mb-12 md:mb-0 md:w-8/12 lg:w-5/12 xl:w-5/12" style={{ position: "relative" }}>
+                <div style={{ fontSize: "20px", border: "solid 1px black", borderRadius: "10px", padding: "5px 20px", position: "absolute", left: "30%" }}>
+                  Hi!</div>
+                <FontAwesomeIcon icon={faUserAstronaut} style={{ fontSize: "200px", color: "#1976d2" }} />
+              </div>
             }
           </div>
         </div>
